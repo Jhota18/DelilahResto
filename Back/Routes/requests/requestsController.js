68204130991da78433f0ce172ed56a4b92d2398e
Jhonatan Gomez <jhonatangomez_18@hotmail.com>
2020-11-19 01:34:59 -0500

    Ajustes realizados

--- Back/Routes/requests/requestsController.js
+++ Back/Routes/requests/requestsController.js
@@ -1,68 +1,107 @@
-const requestsModel = require('../../Database/models/requestsModel');
-const productsModel = require('../../Database/models/productsModel');
-const usersModel = require('../../Database/models/usersModel');
-
+const requestsModel = require("../../Database/models/requestsModel");
+const productsModel = require("../../Database/models/productsModel");
+const usersModel = require("../../Database/models/usersModel");
 
 const createRequest = (requestData) => {
-    return new Promise((res, rejc) => {
-		if (requestData.products && Array.isArray(requestData.products)) {
-			const { products, ...request } = requestData;
-			console.log(request);
-			requestsModel
-				.create(request)
-				.then((request) => {
-					Promise.all(
-						products.map((product) => {
-							return request.addProducts(product.id, { through: { quantity: product.quantity } });
-					   })
-					).then((response) =>{
-						res(response);
-					})
-					.catch( (error) => {
-						console.log(request.id);
-						requestsModel.destroy({ where: { id: request.id } });
-						if(error.name === "SequelizeForeignKeyConstraintError") {
-							rejc({ status: 404, message: 'No se encontró el ID de producto, por favor verifique' });
-						} else{
-							rejc({ status: 500, message: 'Error interno, no se pudo crear la orden' });
-						}
-					})
-				})
-				.catch((error) => {
-					console.log(error);
-					if(error.name === "SequelizeForeignKeyConstraintError") {
-						rejc({ status: 404, message: 'No se encontró el ID de usuario, por favor verifique' });
-					}
-				});
-		} else {
-			rejc({ status: 406, message: 'Campos enviados no validos' });
-		}
-	});
+  return new Promise((res, rejc) => {
+    if (requestData.products && Array.isArray(requestData.products)) {
+      const { products, ...request } = requestData;
+      console.log(request);
+      requestsModel
+        .create(request)
+        .then((request) => {
+          Promise.all(
+            products.map((product) => {
+              return request.addProducts(product.id, {
+                through: { quantity: product.quantity },
+              });
+            })
+          )
+            .then((response) => {
+              res(response);
+            })
+            .catch((error) => {
+              console.log(request.id);
+              requestsModel.destroy({ where: { id: request.id } });
+              if (error.name === "SequelizeForeignKeyConstraintError") {
+                rejc({
+                  status: 404,
+                  message:
+                    "No se encontró el ID de producto, por favor verifique",
+                });
+              } else {
+                rejc({
+                  status: 500,
+                  message: "Error interno, no se pudo crear la orden",
+                });
+              }
+            });
+        })
+        .catch((error) => {
+          console.log(error);
+          if (error.name === "SequelizeForeignKeyConstraintError") {
+            rejc({
+              status: 404,
+              message: "No se encontró el ID de usuario, por favor verifique",
+            });
+          }
+        });
+    } else {
+      rejc({ status: 406, message: "Campos enviados no validos" });
+    }
+  });
 };
-
 
 const updateById = (id, data) => {
-	return new Promise((res, rejc) => {
-		if (data.hasOwnProperty('status') && id) {
-			requestsModel
-				.update({ status: data.status }, { where: { id: id } })
-				.then((response) => {
-					console.log(response);
-					if (response[0] === 1) {
-						res('Estado de el pedido actualizado exitosamente');
-					} else {
-						rejc({ status: 404, message: 'Datos no encontrados, no se pudo actualizar tu pedido.' });
-					}
-				})
-				.catch((error) => {
-					rejc({ status: 500, message: 'Por favor intente mas tarde' });
-				});
-		} else {
-			rejc({ status: 406, message: 'Campos no validos' });
-		}
-	});
+  return new Promise((res, rejc) => {
+    if (data.hasOwnProperty("status") && id) {
+      requestsModel
+        .update({ status: data.status }, { where: { id: id } })
+        .then((response) => {
+          console.log(response);
+          if (response[0] === 1) {
+            res("Estado de el pedido actualizado exitosamente");
+          } else {
+            rejc({
+              status: 404,
+              message: "Datos no encontrados, no se pudo actualizar tu pedido.",
+            });
+          }
+        })
+        .catch((error) => {
+          rejc({ status: 500, message: "Por favor intente mas tarde" });
+        });
+    } else {
+      rejc({ status: 406, message: "Campos no validos" });
+    }
+  });
 };
 
+const deleteById = (id) => {
+  return new Promise((res, rej) => {
+    if (id) {
+      requestsModel
+        .destroy({ where: { id: id } })
+        .then((response) => {
+          if (response === 1) {
+            res("Pedido eliminado");
+          } else {
+            rejc({
+              status: 404,
+              message: `No se encontro el pedido, por favor verifique`,
+            });
+          }
+        })
+        .catch((error) => {
+          rejc({
+            status: 500,
+            message: "Tenemos problemas, por favor intente  mas tarde.",
+          });
+        });
+    } else {
+      rejc({ status: 400, message: "Por favor envie el ID del pedido" });
+    }
+  });
+};
 
-
-module.exports = {createRequest, updateById}
+module.exports = { createRequest, updateById, deleteById };
--- Back/Routes/requests/requestsRoute.js
+++ Back/Routes/requests/requestsRoute.js
@@ -1,36 +1,80 @@
-const express = require('express');
+const express = require("express");
 //CONTROLLERS
-const { createRequest, findById, updateById} = require('./requestsController');
+const {
+  createRequest,
+  updateById,
+  deleteById,
+} = require("./requestsController");
+
+const requestsModel = require("../../Database/models/requestsModel");
+const ordersModel = require("../../Database/models/ordersModel");
+const productsModel = require("../../Database/models/productsModel");
+const usersModel = require("../../Database/models/usersModel");
 
 //MIDDLEWARES
-const authentication = require('../../middlewares/authentication');
-const authorization = require('../../middlewares/authorization');
+const authentication = require("../../middlewares/authentication");
+const authorization = require("../../middlewares/authorization");
+const { route } = require("../users/userRoutes");
 
 // ORDER ROUTES
 const router = express.Router();
 //CREATE ORDER
-router.post('/createR', authentication, (req, res) => {
-	let data = req.body;
-	data.request_date = new Date();
-	data.status = 'nuevo';
-	createRequest(data).then((request)=>{
-		res.status(200).json(request);
-	}).catch((error) => {
-		res.status(error.status).json(error.message);
-	})
+router.post("/createR", authentication, (req, res) => {
+  let data = req.body;
+  data.request_date = new Date();
+  data.status = "nuevo";
+  createRequest(data)
+    .then((request) => {
+      res.status(200).json(request);
+    })
+    .catch((error) => {
+      res.status(error.status).json(error.message);
+    });
 });
 
+router.patch("/updateR/:id", authentication, authorization, (req, res) => {
+  let id = req.params.id;
+  let data = req.body;
+  updateById(id, data)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(error.status).json(error.message);
+    });
+});
 
-router.patch('/updateR/:id', authentication, authorization, (req, res) => {
-	let id = req.params.id;
-	let data = req.body;
-	updateById(id, data)
-		.then((response) => {
-			res.status(200).json(response);
-		})
-		.catch((error) => {
-			res.status(error.status).json(error.message);
-		});
+router.delete("/delete/:id", authentication, authorization, (req, res) => {
+  let id = req.params.id;
+  deleteById(id)
+    .then((response) => {
+      res.status(200).json(response);
+    })
+    .catch((error) => {
+      res.status(error.status).json(error.message);
+    });
+});
+
+router.get("/list", authentication, (req, res) => {
+  requestsModel
+    .findAll()
+    .then((orders) => {
+      res.status(200).json(orders);
+    })
+    .catch((error) => {
+      req.status(500);
+    });
+});
+
+router.get("/orderList", authentication, (req, res) => {
+  ordersModel
+    .findAll()
+    .then((orders) => {
+      res.status(200).json(orders);
+    })
+    .catch((error) => {
+      res.status(500);
+    });
 });
 
 module.exports = router;
